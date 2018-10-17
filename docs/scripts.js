f995fdb0ffa933012a0d2e6018e7c79964707b49
gcsalzburg gcbristol@gmail.com <g.cave@kiska.com>
2018-10-17 22:12:23 +0200

    Implement functioning temperature display

---
+++ docs/scripts.js
@@ -0,0 +1,30 @@
+// Create a client instance
+client = new Paho.MQTT.Client("broker.hivemq.com", 8000, "gcsalzburg"+new Date().getTime());
+
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+// connect the client
+document.getElementById("status").innerHTML = "Connecting...";
+client.connect({onSuccess:onConnect});
+
+// called when the client connects
+function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+  document.getElementById("status").innerHTML = "Subscribing...";
+  client.subscribe("gcsalzburg/temp");
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+    document.getElementById("status").innerHTML = "Connection lost";
+    if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost:"+responseObject.errorMessage);
+    }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+    document.querySelector('#temp').innerHTML = message.payloadString+"°";
+}
